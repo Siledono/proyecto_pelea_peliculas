09219b4d82de06acdfd5a99ccbdf4c758749f25c
Siledono <Siledono100@gmail.com>
2025-03-31 17:24:28 -0700

    Guardando los archivos

--- autocomplete.js
+++ autocomplete.js
@@ -1,3 +1,53 @@
-const createAutocomplete = ({}) => {
+const createAutocomplete = ({root, rederOption, onOptionSelect, inputValue, fetchData}) => {
     
+    root.innerHTML = `
+        <label><b>Busqueda</b></label>
+        <input class="input"/>
+        <div class = "dropdown">
+            <div class = "dropdown-menu">
+                <div class = "dropdown-content results"></div>
+            </div>
+        </div>
+    `   
+}
+
+const input = root.querySelector('input')
+const dropdown = root.querySelector('.dropdown')
+const resultwrapper = root.querySelector('.results')
+
+const debonce = (func, delay = 1000) => {
+    let timeoutId
+    return(...arg) => {
+        clearTimeout(timeoutId)
+        timeoutId = setTimeout(() => {
+            func.apply(null, arg)
+        }, delay)
+    }
+}
+
+const onInput = async event => {
+    const items = await fetchData(event.target.values)
+    console.log("Movies", items)
+ 
+    if(!items.length){
+        dropdown.classList.remove('is-activate')
+        return
+    }
+
+}
+
+resultwrapper.innerHTML = ''
+dropdown.classList.add('is-activate')
+for (let item of items) {
+    const option = document.createElement('a')
+
+    option.classList.add('dropdown-item')
+    option.innerHTML = renderOption(item)
+    option.addEventListener('click', () => {
+        dropdown.classList.remove('is-activate')
+        input.value = inputValue(item)
+        onOptionSelect(item)
+        console.log("onMovieSelect")
+    })
+    resultsWeapper.appendChild(option)
 }
